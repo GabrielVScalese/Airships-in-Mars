020509023ca655c1b5200aad15aee7d714cbc573
Gabriel <gabriel.scalese@hotmail.com>
2020-09-25 22:38:15 -0300

    Created CidadeMarte class

---
+++ Projeto base - apCaminhosMarte/apCaminhosMarte/CidadeMarte.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Metadata.W3cXsd2001;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace apCaminhosMarte
+{
+    // Nome: Gabriel Villar Scalese    RA: 19171
+    // Nome: Nícolas Maisonette Duarte RA: 19192
+    class CidadeMarte
+    {
+        // Atributo que representa o id da cidade
+        private int id;
+        // Atributo que representa o nome da cidade
+        private string nomeCidade;
+        // Atributo que representa a coordenada x da cidade
+        private int x;
+        // Atributo que representa a coordenada y da cidade
+        private int y;
+
+        // Construtor da classe
+        public CidadeMarte (int id, string nomeCidade, int x, int y)
+        {
+            Id = id;
+            NomeCidade = nomeCidade;
+            X = x;
+            Y = y;
+        }
+
+        // Propriedade do atributo id
+        public int Id
+        {
+            get => id;
+            set
+            {
+                if (id < 0)
+                    throw new Exception("Id invalido");
+
+                id = value;
+            }
+        }
+
+        // Propriedade do atributo nomeCidade
+        public string NomeCidade
+        {
+            get => nomeCidade;
+            set
+            {
+                if (nomeCidade == null || nomeCidade.Equals(""))
+                    throw new Exception("Nome de cidade invalido");
+
+                nomeCidade = value;
+            }
+        }
+
+        // Propriedade do atributo x
+        public int X
+        {
+            get => x;
+            set
+            {
+                x = value;
+            }
+        }
+
+        // Propriedade do atributo y
+        public int Y
+        {
+            get => y;
+            set
+            {
+                y = value;
+            }
+        }
+    }
+}
