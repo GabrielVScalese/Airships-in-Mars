5a57bd1086fd857cae6c82f94a4ebebe4671346d
Gabriel <gabriel.scalese@hotmail.com>
2020-09-26 11:51:47 -0300

    Update

---
+++ Projeto base - apCaminhosMarte/apCaminhosMarte/ListaSimples.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms.VisualStyles;
+
+namespace apCaminhosMarte
+{
+    class ListaSimples<Dado> where Dado : IComparable<Dado>
+    {
+        private class No
+        {
+            private Dado info;
+            private No prox;
+
+            public No (Dado info, No prox)
+            {
+                Info = info;
+                Prox = prox;
+            }
+
+            public No (Dado info)
+            {
+                Info = info;
+            }
+
+            public Dado Info
+            {
+                get => info;
+                set
+                {
+                    info = value;
+                }
+            }
+
+            public No Prox
+            {
+                get => prox;
+                set
+                {
+                    prox = value;
+                }
+            }
+        }
+
+        private No primeiro, ultimo;
+
+        public ListaSimples ()
+        { }
+
+
+        public void InserirNoFim (Dado info)
+        {
+            if (info == null)
+                throw new Exception("Informacao invalida");
+
+            if (IsVazia())
+            {
+                primeiro = new No(info, primeiro);
+                ultimo = primeiro;
+            }
+            else
+            {
+                No aux = new No(info, null);
+                ultimo.Prox = aux;
+                ultimo = aux;
+            }
+        }
+
+        public void RemoverDoFim ()
+        {
+            if (IsVazia())
+                throw new Exception("Nada a remover");
+
+            if (GetQtd() == 1)
+            {
+                primeiro = null;
+                ultimo = null;
+            }
+            else
+            {
+                No aux = primeiro;
+                while (aux != null)
+                {
+                    if (aux.Prox.Equals(ultimo))
+                    {
+                        aux.Prox = null;
+                        ultimo = aux;
+                        break;
+                    }
+
+                    aux = aux.Prox;
+                }
+            }
+        }
+
+        public bool IsVazia ()
+        {
+            return primeiro == null;
+        }
+
+        public int GetQtd ()
+        {
+            int qtd = 0;
+            No aux = primeiro;
+            while (aux != null)
+            {
+                qtd++;
+                aux = aux.Prox;
+            }
+
+            return qtd;
+        }
+
+        public Dado GetDoFim ()
+        {
+            if (IsVazia())
+                throw new Exception("Lista vazia");
+
+            return ultimo.Info;
+        }
+    }
+}
---
+++ Projeto base - apCaminhosMarte/apCaminhosMarte/PilhaLista.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace apCaminhosMarte
+{
+    class PilhaLista<Dado> where Dado : IComparable<Dado>
+    {
+        private ListaSimples<Dado> listaSimples;
+
+        public PilhaLista ()
+        { }
+
+        public void Empilhar (Dado info)
+        {
+            listaSimples.InserirNoFim(info);
+        }
+
+        public void Desempilhar ()
+        {
+            listaSimples.RemoverDoFim();
+        }
+
+        public Dado Topo
+        {
+            get => listaSimples.GetDoFim();
+        }
+    }
+}
